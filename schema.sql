5624065aaac7da6ed533dc51b3aff69bd37ac0f3
Owen <owenedwards422@gmail.com>
2020-03-12 14:51:14 -0400

    added post and put routes

--- schema.sql
+++ schema.sql
@@ -4,6 +4,7 @@ USE burgify_me;
 
 CREATE TABLE burgers (
     id INT AUTO_INCREMENT NOT NULL,
+    PRIMARY KEY (id),
     sandwich VARCHAR(30) NOT NULL,
     devoured BOOLEAN NOT NULL DEFAULT 0
 );
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
@@ -20,12 +23,40 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    afterConnection();
 });
 
-function afterConnection(){
-    connection.end();
-};
+app.get('/', function(req, res){
+    connection.query(
+        "SELECT * FROM burgers",
+        function(err, data){
+            if(err) throw err;
+            res.render('index', {food: data})
+        }
+    )
+});
+
+app.post('/api', function(req, res){
+    connection.query(
+        "INSERT INTO burgers SET ?",
+        req.body,
+        function(err, result){
+            if(err) throw err;
+            res.sendStatus(200);
+        }
+    )
+});
+
+app.put('/api/:id', function(req, res){
+    let id = req.params.id
+    connection.query(
+        "UPDATE burgers SET devoured = true WHERE id = ?",
+        id,
+        function(err, result){
+            if(err) throw err;
+            res.sendStatus(200);
+        }
+    )
+})
 
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT); 
